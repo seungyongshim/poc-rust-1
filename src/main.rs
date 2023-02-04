4ee1a542bf485ac7f5ed3b751369fb561fc7c768
SeungYongShim <syshim@me.com>
2023-02-04 10:51:52 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,21 @@ fn main() {
     let tuple = ('E', 5i32, true);
 
     println!("{}", tuple.0);
+
+    let user_1 = Student
+    {
+        name: String::from("Shim"),
+        level: 2,
+        remote: true
+    };
+
+    println!("{}", user_1.name);
+}
+
+
+struct Student
+{
+    name : String,
+    level: u8,
+    remote: bool
 }

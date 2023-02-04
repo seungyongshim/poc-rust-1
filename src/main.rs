1315c04751ebfe873102a96a255f4933fabddcb2
SeungYongShim <syshim@me.com>
2023-02-04 11:03:29 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
 fn main() {
-    let we_load = WebEvent::WELoad(true);
-
-    let click = MouseClick {x : 100, y: 250};
-
-    let we_click = WebEvent::WEClick(click);
-
-    let keys = KeyPress(String::from("Ctrl+"), 'N');
-
-    let we_key = WebEvent::WEKeys(keys);
-
-    println!("{:#?}", we_click);
+   let a = goodbye("Shim");
+   println!("{}", a);
 }
 
-struct Student
-{
-    name : String,
-    level: u8,
-    remote: bool
-}
-
-#[derive(Debug)]
-struct KeyPress(String, char);
-
-#[derive(Debug)]
-struct MouseClick{ x: i64, y: i64}
-
-#[derive(Debug)]
-enum WebEvent {
-    WELoad(bool),
-    WEKeys(KeyPress),
-    WEClick(MouseClick)
+fn goodbye(message: &str) -> u32 {
+    println!("Goodbye. {}", message);
+    32
 }

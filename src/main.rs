6250e1ae6353331dec9cc2e0cd42bb02e03172e0
SeungYongShim <syshim@me.com>
2023-02-04 11:07:38 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,17 @@ fn goodbye(message: &str) -> u32 {
     println!("Goodbye. {}", message);
     32
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod add_function_tests {
+    use super::add;
+
+    #[test]
+    fn add_works() {
+        assert_eq!(add(1,2), 3);
+    }
+}

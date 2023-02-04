120d403ed46f6424c9f172b90e2a15f289e6ce24
SeungYongShim <syshim@me.com>
2023-02-04 10:41:20 +0900

    .

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a_number = 10;
+    let a_word = "Ten";
+
+    let a_number = a_number + 11;
+
+    println!("The number is {}.", a_number);
+    println!("The word is {}.", a_word);
+}
